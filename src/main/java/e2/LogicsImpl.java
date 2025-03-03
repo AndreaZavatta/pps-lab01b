4f89758afbe326bb09b8a88e82b4fd85bd8a3232
Andrea Zavatta <zavattaandrea@gmail.com>
2025-03-03 18:09:39 +0100

    create tests for logic class

--- src/main/java/e2/LogicsImpl.java
+++ src/main/java/e2/LogicsImpl.java
@@ -8,14 +8,20 @@ public class LogicsImpl implements Logics {
 	private Pair<Integer,Integer> knight;
 	private final Random random = new Random();
 	private final int size;
-	 
+
+	public LogicsImpl(int size, Pair<Integer, Integer> pawnPosition, Pair<Integer, Integer> knightPosition){
+        this.pawn = pawnPosition;
+        this.knight = knightPosition;
+		this.size = size;
+    }
+
     public LogicsImpl(int size){
     	this.size = size;
         this.pawn = this.randomEmptyPosition();
         this.knight = this.randomEmptyPosition();	
     }
     
-	private final Pair<Integer,Integer> randomEmptyPosition(){
+	private Pair<Integer,Integer> randomEmptyPosition(){
     	Pair<Integer,Integer> pos = new Pair<>(this.random.nextInt(size),this.random.nextInt(size));
     	// the recursive call below prevents clash with an existing pawn
     	return this.pawn!=null && this.pawn.equals(pos) ? randomEmptyPosition() : pos;
--- src/test/java/e2/LogicTest.java
+++ src/test/java/e2/LogicTest.java
@@ -1,15 +1,43 @@
 package e2;
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LogicTest {
 
-  @Test
-  public void test() {
-    assert(true);
-    // TODO: Add your test logic here
-    // You can generate random inputs and assert the expected output
-    // For example:
-    // int result = Logic.someMethod(5, 10);
-    // assertEquals(expectedResult, result);
+  Logics logics;
+
+  @BeforeEach
+  void setup() {
+    logics = new LogicsImpl(8, new Pair<Integer, Integer>(5,5), new Pair<Integer, Integer>(6,7));
   }
+
+  @Test
+  public void testPawnCreation() {
+    assertTrue(logics.hasPawn(5,5));
+  }
+
+  @Test
+  public void testKnightCreation(){
+    assertTrue(logics.hasKnight(6,7));
+  }
+
+  @Test
+  public void testPawnCapture(){
+    assertTrue(logics.hit(5,5));
+  }
+
+  @Test
+  public void testKnightMove(){
+    assertFalse(logics.hit(7,5));
+  }
+
+
+  @Test
+  public void testIndexOutOfBoundsException(){
+    assertThrows(IndexOutOfBoundsException.class, ()->logics.hit(8,9));
+  }
+
+
+
 }
